85cd81419126f59863980cb625b52bc76e9ec232
Kalyanis0012 <kalyanisable0123@gmail.com>
2026-08-13 18:10:53 +0530

    example added

---
+++ src/WrapperClasses/CombinedEx.java
@@ -0,0 +1,13 @@
+package WrapperClasses;
+public class CombinedEx {
+    static void main() {
+    int a=10;
+    Integer obj=a;
+    int b=obj;
+    System.out.println("Primitive: "+ a);
+    System.out.println("Wrapper: " +obj);
+    System.out.println("Back to primitve: "+b);
+
+
+}
+}
