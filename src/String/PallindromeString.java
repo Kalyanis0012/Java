191c33af10e17e388b1da6fa002fb7040f79a946
Kalyanis0012 <kalyanisable0123@gmail.com>
2026-08-14 17:30:46 +0530

    pallindrome example added

--- src/String/PallindromeString.java
+++ src/String/PallindromeString.java
@@ -1,4 +1,22 @@
-package String;
+
+
+import java.util.Scanner;
 
 public class PallindromeString {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter String");
+        String s=sc.nextLine();
+        String reverse="";
+        for(int i= s.length()-1;i>=0;i--){
+            reverse=reverse+s.charAt(i);
+        }
+        System.out.println("Original = " + s);
+        System.out.println("Reverse = " + reverse);
+        if(s.equals(reverse)){
+            System.out.println("String is Pallindrome");
+        }else{
+            System.out.println("String is not Pallindrome");
+        }
+    }
 }
