061fa63e939b5031e3564add48583dfa4b9fc353
Kalyanis0012 <kalyanisable0123@gmail.com>
2026-08-14 17:39:02 +0530

    program added

--- src/String/CountWords.java
+++ src/String/CountWords.java
@@ -1,4 +1,12 @@
-package String;
+import java.util.Scanner;
 
 public class CountWords {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter String:");
+        String s=sc.nextLine();
+        String[] words=s.trim().split("\\s+");
+        System.out.println("Number of words in given string: "+words.length);
+
+    }
 }
