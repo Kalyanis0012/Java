1f6529c12706132c83355048c8dbad0599ee223e
Kalyanis0012 <kalyanisable0123@gmail.com>
2026-08-20 17:16:17 +0530

    runtime polymorphism ex added

--- src/OOPs/Polymorphism/RuntimePolymorphism.java
+++ src/OOPs/Polymorphism/RuntimePolymorphism.java
@@ -1,4 +1,30 @@
-package OOPs.Polymorphism;
+class Payment{
+    void pay(){
+        System.out.println("Making payment");
+    }
+}
+
+class UPI extends Payment{
+    @Override
+    void pay(){
+        System.out.println("Payment by UPI");
+    }
+}
+
+class Card extends Payment{
+    @Override
+    void pay(){
+        System.out.println("Payment done by card");
+    }
+}
 
 public class RuntimePolymorphism {
+    public static void main(String[] args){
+        Payment p;
+        p=new UPI();
+        p.pay();
+
+        p=new Card();
+        p.pay();
+    }
 }
