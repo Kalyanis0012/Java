3fc000a03b6ab5b9ea0a5d9780ecd818b50dd0ad
Kalyanis0012 <kalyanisable0123@gmail.com>
2026-09-23 16:16:38 +0530

    prime number code added

--- src/BasicPrograms/PrimeNumber.java
+++ src/BasicPrograms/PrimeNumber.java
@@ -1,4 +1,21 @@
-package BasicPrograms;
-
-public class PrimeNumber {
+class primeNumber{
+    public static void main(String[] args){
+        int num=17;
+        boolean prime=true;
+        if(num<=1){
+            prime=false;
+        }else{
+            for(int i=2;i<num;i++){
+                if(num%i==0){
+                    prime=false;
+                    break;
+                }
+            }
+        }
+        if(prime){
+            System.out.println("Prime number");
+        }else {
+            System.out.println("not a prime number");
+        }
+    }
 }
